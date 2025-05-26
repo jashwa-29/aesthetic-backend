9f49071161dc320d4c8f7f0c06af2d51c3856b2e
jashwa-29 <153584496+jashwa-29@users.noreply.github.com>
2025-05-26 13:48:45 +0530

    Initial commit

--- server.js
+++ server.js
@@ -73,5 +73,5 @@ app.use((err, req, res, next) => {
 // --- Start Server ---
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`🚀 hello Server running on port ${PORT}`);
 });
